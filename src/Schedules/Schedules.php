efc81f416a26983d51bbc96bc289183063124efe
danilsirotalol <danilsirotalol@gmail.com>
2021-01-05 13:16:00 +0200

    close #1

--- src/Schedules/Schedules.php
+++ src/Schedules/Schedules.php
@@ -4,6 +4,12 @@ namespace Otis22\VetmanagerConversations\Schedules;
 
 use GuzzleHttp\Client;
 use Otis22\VetmanagerToken\Token;
+use Otis22\VetmanagerRestApi\Query\Filter\MoreThan;
+use Otis22\VetmanagerRestApi\Query\Filter\LessThan;
+use Otis22\VetmanagerRestApi\Model\Property;
+use Otis22\VetmanagerRestApi\Query\Filter\Value\StringValue;
+use Otis22\VetmanagerRestApi\Query\Filter\EqualTo;
+use Otis22\VetmanagerRestApi\Query\Filters;
 
 use function Otis22\VetmanagerRestApi\uri;
 
@@ -39,32 +45,32 @@ class Schedules
     public function byIntervalInDays($days = 7, $doctor_id = 0, $clinic_id = 0): array
     {
         $now = date("Y-m-d");
-        $filter_query[] = [
-            "property" => "end_datetime",
-            "value" => date('Y-m-d', intval(strtotime($now . " +" . $days . " days"))) . " 23:59:59",
-            "operator" => "<=",
-        ];
+        $filteringParams[] = new MoreThan(
+            new Property('begin_datetime'),
+            new StringValue($now . " 00:00:00")
+        );
+        $filteringParams[] = new LessThan(
+            new Property('end_datetime'),
+            new StringValue(date('Y-m-d', intval(strtotime($now . " +" . $days . " days"))) . " 23:59:59")
+        );
         if ($doctor_id) {
-            $filter_query[] = [
-                "property" => 'doctor_id',
-                "value" => $doctor_id,
-                "operator" => "=",
-            ];
+            $filteringParams[] = new EqualTo(
+                new Property('doctor_id'),
+                new StringValue(strval($doctor_id))
+            );
         }
         if ($clinic_id) {
-            $filter_query[] = [
-                "property" => 'clinic_id',
-                "value" => $clinic_id,
-                "operator" => "=",
-            ];
+            $filteringParams[] = new EqualTo(
+                new Property('clinic_id'),
+                new StringValue(strval($clinic_id))
+            );
         }
+        $filters = new Filters(...$filteringParams);
         $request = $this->httpClient->request(
             'GET',
             uri("timesheet")->asString(),
             [
-                "query" => [
-                    "filter" => json_encode($filter_query)
-                ]
+                "query" => $filters->asKeyValue()
             ]
         );
         $result = json_decode(
